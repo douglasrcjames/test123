89b6ca1f9b65394ae82a22c9b55f758b0c0c60ff
Douglas James <douglasrcjames@gmail.com>
2023-04-28 21:48:28 -0700

    Added custom city input
    vince u see this?

--- src/ComponentDoug.js
+++ src/ComponentDoug.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { doc, setDoc } from "firebase/firestore"; 
 import { firestore } from './Fire';
 
 export default function ComponentDoug() {
+    const [cityName, setCityName] = useState("San Francisco");
 
-    const clickToSetCity = (cityName) => {
-        console.log("cityName: " + cityName);
-        setDoc(doc(firestore, "cities", cityName), {
-            name: cityName,
+    const clickToSetCity = (incCityName) => {
+        console.log("incCityName: " + incCityName);
+        setDoc(doc(firestore, "cities", incCityName), {
+            name: incCityName,
             state: "CA",
             country: "USA"
         });
@@ -16,7 +17,12 @@ export default function ComponentDoug() {
   return (
     <div>
         <h2>ComponentDoug</h2>
-        <button onClick={() => clickToSetCity("San Fran")}>click to set</button>
+        <input 
+            type="text"
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+        />
+        <button onClick={() => clickToSetCity(cityName)}>click to set {cityName}</button>
     </div>
   )
 }
