fbaa891f4a60cd19961d9c33cfc8bae56b2b2ede
Douglas James <douglasrcjames@gmail.com>
2023-04-29 00:45:03 -0700

    Persons looping map

--- src/ComponentDoug.js
+++ src/ComponentDoug.js
@@ -2,7 +2,40 @@ import React, { useState } from 'react'
 import { doc, setDoc } from "firebase/firestore"; 
 import { firestore } from './Fire';
 
+
 export default function ComponentDoug() {
+    let persons = [
+        {
+            name: "Doug", 
+            age: 55,
+            address: {
+                street: "123 Main St",
+                city: "San Francisco",
+                state: "CA",
+                zip: "94101"
+            },
+        },
+        {
+            name: "Sue", 
+            age: 44,
+            sex: "female",
+            alive: false,
+        },
+        {
+            name: "Bob", 
+            age: 33
+        },
+        {
+            name: "Vince", 
+            age: 11
+        },
+        {
+            name: "Jill", 
+            age: 22
+        },
+    ];
+
+
     const [cityName, setCityName] = useState("San Francisco");
 
     const clickToSetCity = (incCityName) => {
@@ -17,12 +50,31 @@ export default function ComponentDoug() {
   return (
     <div>
         <h2>ComponentDoug</h2>
-        <input 
+        <input
+            id="my-id"
             type="text"
             value={cityName}
             onChange={(e) => setCityName(e.target.value)}
         />
         <button onClick={() => clickToSetCity(cityName)}>click to set {cityName}</button>
+        <br/>
+        <hr />
+        <br/>
+        <h2>Persons:</h2>
+        <ul>
+            {
+                persons.map((person, index) => {
+                    return (
+                        <li style={{margin: "20px 0 0 0"}}>
+                            <ul>
+                                <li><b>Name:</b> {persons[index].name}</li>
+                                <li><b>Age:</b> {persons[index].age}</li>
+                            </ul>
+                        </li>
+                    )
+                })
+            }
+        </ul>
     </div>
   )
 }
